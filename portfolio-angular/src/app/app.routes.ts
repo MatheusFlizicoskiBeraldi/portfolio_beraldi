7fd86a6fdb7e6dd0b68bdae6fff06d78d52ae5d0
MONOKAYN7865 <20241CTB0100027@estudantes.ifpr.edu.br>
2026-09-21 19:59:08 +0000

    Aula17: API de projetos e catalogo (MariaDB + JSON)

--- portfolio-angular/src/app/app.routes.ts
+++ portfolio-angular/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
+import { HomeComponent } from './home/home';
 import { Sobre } from './sobre/sobre';
 import { Projetos } from './projetos/projetos';
-import { Contato } from './contato/contato';
+import { ContatoComponent } from './contato/contato';
 
 export const routes: Routes = [
-    { path: '', component: Home}, 
+    { path: '', component: HomeComponent}, 
     { path: 'sobre', component: Sobre}, 
     { path: 'projetos', component: Projetos}, 
-    { path: 'contato', component: Contato}, 
+    { path: 'contato', component: ContatoComponent}, 
        
 ];
